7016273ba13071e89703bb48ed6359b48337a9ae
Sagar Agrawal <sagarscience708@gmail.com>
2025-07-22 01:19:13 +0530

    update vite.config.js

--- server.js
+++ server.js
@@ -120,10 +120,7 @@ app.use(express.json());
 
 // 🔗 MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
